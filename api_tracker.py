7d34ec3a46e36f86f14c371ccaa47f1c53e4f5db
Corey Soreff <csoreff@gmail.com>
2015-08-13 13:33:57 -0400

    Wrapped everything in a class and put the file as an instance variable

--- api_tracker.py
+++ api_tracker.py
@@ -1,37 +1,56 @@
 import re
 
-api_data = open('elblogs.txt')
+class ApiTracker:
 
-partner_info = {'partner': '/callback/nyr8nx', 'campaign_id': 'cid=X9KN0'}
-callback_count = 0
-campaign_callback_count = 0
+    def __init__(self, api_data_file):
+        self.api_data = open(api_data_file)
 
-for line in api_data:
-    if partner_info['partner'] in line:
-        callback_count += 1
-    if partner_info['partner'] in line and partner_info['campaign_id'] in line:
-        campaign_callback_count += 1
+    def find_partner_callback_count(self, partner_name):
+        partner = "/callback/{}".format(partner_name)
+        callback_count = 0
+        self.api_data.seek(0)
+        for line in self.api_data:
+            if partner in line:
+                callback_count += 1
 
-print callback_count
-print campaign_callback_count
+        return callback_count
 
-def count_api_requests():
-    requests = {}
-    api_data = open('elblogs.txt')
-    for line in api_data:
-        if 'api/' in line:
-            api_request = re.search('api/\S*', line).group(0).strip()
-            if api_request in requests:
-                requests[api_request] += 1
-            else:
-                requests[api_request] = 1
-    return requests
 
-def total_api_requests(requests):
-    return sum(requests.values())
+    def find_partner_campaign_callback_count(self, partner_name, campaign_id):
+        partner_info = {
+            'partner': "/callback/{}".format(partner_name),
+            'campaign_id': "cid={}".format(campaign_id)
+        }
+        campaign_callback_count = 0
+        self.api_data.seek(0)
+        for line in self.api_data:
+            if partner_info['partner'] in line and partner_info['campaign_id'] in line:
+                campaign_callback_count += 1
 
-api_requests = count_api_requests()
+        return campaign_callback_count
+
+
+    def count_api_requests(self):
+        requests = {}
+        self.api_data.seek(0)
+        for line in self.api_data:
+            if 'api/' in line:
+                api_request = re.search('api/\S*', line).group(0).strip()
+                if api_request in requests:
+                    requests[api_request] += 1
+                else:
+                    requests[api_request] = 1
+
+        return requests
+
+    @staticmethod
+    def total_api_requests(requests):
+        return sum(requests.values())
+
+api_tracker = ApiTracker('elblogs.txt')
+api_requests = api_tracker.count_api_requests()
 for k, v in api_requests.iteritems():
     print k + ': ' + str(v)
-
-print 'Total api requests: ' + str(total_api_requests(count_api_requests()))
+print api_tracker.find_partner_callback_count('nyr8nx')
+print api_tracker.find_partner_campaign_callback_count('nyr8nx', 'X9KN0')
+print 'Total api requests: ' + str(api_tracker.total_api_requests(api_requests))
